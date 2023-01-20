5668e0d49173d77deae6dc7c07e9712b24c3d748
Renanppcosta <renanppcosta@gmail.com>
2023-01-20 12:22:44 -0300

    Add Spectre.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+import "spectre.css/dist/spectre.min.css"
+import "spectre.css/dist/spectre-icons.min.css"
 
 createApp(App).mount('#app')
