2c7feb8b52e5622cf60473045aa7956b6eb3ec33
Renanppcosta <renanppcosta@gmail.com>
2023-01-21 14:33:42 -0300

    API added and page almost finished

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div class="container grid p-2">
+    <div class="card p-2">
+      <h1 class="h1">Conversor de Moedas</h1>
+    </div>
+    <Coins :coins="coins"/>
   </div>
 </template>
 
 <script>
+import { onMounted, reactive, toRefs } from "vue"
+import api from "./services/api"
+import Coins from "./components/Coins.vue"
 
+export default {
+  name: "App",
+  components: {Coins},
+  setup(){
+
+    const data = reactive({
+      coins: {}
+    });
+
+    onMounted(async ()=>{
+      const res = await api.all();
+
+      data.coins = res.data
+    })
+
+    return { ...toRefs(data) }
+  }
+}
 </script>
 
 
---
+++ src/components/Coins.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+    <div class="card" v-for="(coin, key) in coins" :key="key">
+        <div class="card-header">
+            <div class="card-title h3">{{coin.name}}</div>
+            <div class="card-subtitle text-gray">{{key}}</div>
+        </div>
+        <div class="card-body">
+            <label for="value">O valor da {{coin.name}} é : </label>
+            <input type="number" class="value" name="value" v-model="value">
+            <span class="h5">{{key}} {{value * coin.high}}</span>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            value: ""
+        }
+    },
+    props: {coins: {type: Object, required: true} }
+};
+
+
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+
+.card{
+    width: 30%;
+    margin: 1rem;
+}
+
+.card-body{
+    display: flex;
+    flex-direction: column;
+}
+
+input{
+    width: 40%;
+    margin-top: 0.3rem;
+}
+@media(max-width:900px){
+    .card{
+        width: 100%;
+    }
+}
+</style>
